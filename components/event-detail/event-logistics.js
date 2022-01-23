aba8dc2d7c509c1aba8ee69000faaac94ca35042
Chisom <chisomije92@gmail.com>
2022-01-23 12:02:55 +0100

    optimize images in evt-item component

--- components/event-detail/event-logistics.js
+++ components/event-detail/event-logistics.js
@@ -16,7 +16,8 @@ function EventLogistics(props) {
   return (
     <section className={classes.logistics}>
       <div className={classes.image}>
-        <img src={`/${image}`} alt={imageAlt} />
+        <Image src={`/${image}`} alt={imageAlt} width height />
+        {/* <img src={`/${image}`} alt={imageAlt} /> */}
       </div>
       <ul className={classes.list}>
         <LogisticsItem icon={DateIcon}>
