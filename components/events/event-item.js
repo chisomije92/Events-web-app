6d2af85340aa12e1378e4635e650f8690825c887
Chisom <chisomije92@gmail.com>
2022-01-23 12:03:55 +0100

    optimize image in evt-item component

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -3,6 +3,7 @@ import AddressIcon from "../icons/address-icon";
 import ArrowRightIcon from "../icons/arrow-right-icon";
 import Button from "../UI/Button";
 import classes from "./event-item.module.css";
+import Image from "next/image";
 const EventItem = (props) => {
   const { title, image, date, location, id } = props;
   const parsedDate = new Date(date).toLocaleDateString("en-US", {
@@ -15,7 +16,7 @@ const EventItem = (props) => {
 
   return (
     <li className={classes.item}>
-      <img src={"/" + image} alt={title} />
+      <Image src={"/" + image} alt={title} width={260} height={160} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
