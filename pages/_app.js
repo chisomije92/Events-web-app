2a77952927b3fba8e7046d33f594d6a2466242b3
Chisom <chisomije92@gmail.com>
2022-01-23 11:31:37 +0100

    add head to app js page

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Events For You</title>
+        <meta
+          name="viewport"
+          contents="initial-scale=1.0, width=device-width"
+        />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
